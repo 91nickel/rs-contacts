b0d9c0bbb6985f5a5b96f233b33f86560d9e8ae5
91nickel <91nickel@gmail.com>
2024-09-11 16:38:31 +0300

    server

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:18
+
+RUN mkdir /tmp/app /app
+WORKDIR /tmp/app
+
+RUN git clone https://github.com/91nickel/rs-contacts.git .
+RUN npm i && npm run build
+
+RUN mv /tmp/app/server.js /app/server.js
+RUN mv /tmp/app/build /app/build
+RUN mv /tmp/app/public /app/public
+
+EXPOSE 3000
+
+WORKDIR /app
+
+CMD ["node", "/app/server.js"]
+
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const PORT = 3000
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('Prod ...')
+    app.use('/', express.static(__dirname))
+    const indexPath = path.join(__dirname, 'public', 'index.html')
+    app.get('*', (request, response) => {
+        return response.sendFile(indexPath)
+    })
+} else {
+    console.log('Dev ...')
+}
+
+async function start () {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server has been started on port ${PORT}...`)
+        })
+    } catch (error) {
+        console.error(error.message)
+        process.exit(1)
+    }
+}
+
+start()
